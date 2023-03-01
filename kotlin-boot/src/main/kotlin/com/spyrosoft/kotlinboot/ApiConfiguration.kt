6a9884905f1e28db8d8f948097ccbdf6d30300fd
mju-spyrosoft <mju@spyro-soft.com>
2023-03-01 01:39:48 +0100

    Remove kotlin boot ver 1

--- kotlin-boot/src/main/kotlin/com/spyrosoft/kotlinboot/ApiConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package com.spyrosoft.kotlinboot
-
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@ConfigurationProperties(prefix = "externalapi")
-class ApiConfiguration(
-    val clientId: String,
-    val url: String
-)
--- kotlin-boot/src/main/kotlin/com/spyrosoft/kotlinboot/db/UserJpaRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.spyrosoft.kotlinboot.jpa
-
-import com.spyrosoft.kotlinboot.db.User
-import org.springframework.data.repository.CrudRepository
-
-interface UserJpaRepository : CrudRepository<User, Long>
-
-
